32a93d0052f6ed3498d16c256086666a276f319b
wirycapybara <w.t.tomasz@gmail.com>
2018-03-18 20:11:58 +0100

    add animated login

--- login/main.js
+++ login/main.js
@@ -34,3 +34,14 @@ function displayNextImage() {
      			cont = 0;
      		}
      }
+    // show/hide password
+//    $(".toggle-password").click(function() {
+
+//  $(this).toggleClass("material-icons");
+  //var input = $($(this).attr("toggle"));
+//  if (input.attr("type") == "password") {
+//    input.attr("type", "text");
+//  } else {
+//    input.attr("type", "password");
+//  }
+//});
